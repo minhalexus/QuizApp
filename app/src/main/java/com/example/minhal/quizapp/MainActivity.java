49c2124cf93a201b9be9040442baad130291c226
wingezda <wingezda@example.com>
2018-07-20 18:35:49 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/minhal/quizapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.minhal.quizapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
